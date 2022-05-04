8d0b076b99bf61915410657effa32a758b3ba1f1
Antonela Pernuzzi <antonelapernuzzi@gmail.com>
2022-05-03 22:56:15 -0300

    estilos textField

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
 import { Context } from "../Context/CartContext";
 import { Link } from "react-router-dom";
-import { collection, getFirestore ,addDoc} from "firebase/firestore";
+import { collection, getFirestore, addDoc } from "firebase/firestore";
 export default function () {
   const { totalPrice } = React.useContext(Context);
   const { cart } = React.useContext(Context);
@@ -14,7 +16,6 @@ export default function () {
   const [surName, setSurname] = useState("");
   const [email, setEmail] = useState("");
   const [adress, setAdress] = useState("");
-  const [city, setCity] = useState("");
   const [orderId, setOrderId] = useState("");
   const [purchaseDone, setPurchaseDone] = useState(false);
 
@@ -31,8 +32,6 @@ export default function () {
         email: email,
 
         adress: adress,
-
-        city: city,
       },
 
       items: [...cart],
@@ -44,26 +43,24 @@ export default function () {
 
     const dataBase = getFirestore();
 
-    const ordersCollection = collection( dataBase, 'orders');
+    const ordersCollection = collection(dataBase, "orders");
 
+    addDoc(ordersCollection, newOrder).then((value) => {
+      setPurchaseDone(true);
 
-    addDoc(ordersCollection,newOrder).then((value)=>{
-        setPurchaseDone(true);
+      setOrderId(value.id);
 
-        setOrderId(value.id);
-    
-        console.log(value.id);
+      console.log(value.id);
     });
     console.log(newOrder);
   }
 
-//   useEffect(() => {
-//     console.log(email, surName, city);
-//   }, [email, surName, city]);
+  //   useEffect(() => {
+  //     console.log(email, surName, city);
+  //   }, [email, surName, city]);
 
   return (
     <>
-      
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
         <Paper
           variant="outlined"
@@ -75,42 +72,74 @@ export default function () {
 
           {purchaseDone === false ? (
             <>
-              Completa con tus datos para finalizar tu compra
-              <input
-                type={"text"}
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />
-              <input
-                type={"text"}
-                value={surName}
-                onChange={(e) => {
-                  setSurname(e.target.value);
-                }}
-              />
-              <input
-                type={"text"}
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-              <input
-                type={"text"}
-                value={adress}
-                onChange={(e) => {
-                  setAdress(e.target.value);
-                }}
-              />
-              <input
-                type={"text"}
-                value={city}
-                onChange={(e) => {
-                  setCity(e.target.value);
-                }}
-              />
+              <React.Fragment>
+                Completa con tus datos para finalizar tu compra
+              </React.Fragment>
+              <Grid container spacing={3}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="name"
+                    name="name"
+                    label="Nombre"
+                    fullWidth
+                    autoComplete="family-name"
+                    variant="standard"
+                    value={name}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="surName"
+                    name="surName"
+                    label="Apellido"
+                    fullWidth
+                    autoComplete="family-name"
+                    variant="standard"
+                    value={surName}
+                    onChange={(e) => {
+                      setSurname(e.target.value);
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="email"
+                    name="email"
+                    label="Email"
+                    fullWidth
+                    autoComplete="family-name"
+                    variant="standard"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    id="adress"
+                    name="adress"
+                    label="Dirección"
+                    fullWidth
+                    autoComplete="family-name"
+                    variant="standard"
+                    value={adress}
+                    onChange={(e) => {
+                      setAdress(e.target.value);
+                    }}
+                  />
+                </Grid>
+              </Grid>
               <Button
                 variant="contained"
                 onClick={savePurchase}
@@ -122,15 +151,11 @@ export default function () {
           ) : (
             <>
               ¡Felicitaciones! Tu compra fue realizada con éxito.
-              
               {name} Gracias por tu compra. Te enviaremos un mail a {email} con
               todos los detalles de compra y envío. El código de tu operación
               es: {orderId}
               <Link to={"/"}>
-                <Button
-                  variant="contained"
-                  sx={{ mt: 3, ml: 1 }}
-                >
+                <Button variant="contained" sx={{ mt: 3, ml: 1 }}>
                   Volver
                 </Button>
               </Link>
